fd7b780ea49e46d09fa35f1db8adee6c6ec18ba3
Amish <amish.tufail2002@gmail.com>
2024-08-06 09:01:26 +0500

    Initial Commit

---
+++ ShareToInsta/ShareToInstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareToInstaApp.swift
+//  ShareToInsta
+//
+//  Created by Amish on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShareToInstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
